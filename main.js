22c872b3f419a9ba9d75d4d6cdfd05feacc313f1
DefyJH <sek322@naver.com>
2024-04-05 17:24:48 +0900

    mainjs start

---
+++ main.js
@@ -0,0 +1,25 @@
+document.querySelector('.musinsa').addEventListener(
+    'click',()=>{
+        location.href='https://www.musinsa.com/'
+    }
+)
+document.querySelector('.kream').addEventListener(
+    'click', ()=>{
+        location.href='https://kream.co.kr/'
+    }
+)
+document.querySelector('.balaan').addEventListener(
+    'click', ()=>{
+        location.href='https://www.balaan.co.kr/shop/main/index.php'
+    }
+)
+document.querySelector('.thunder').addEventListener(
+    'click', ()=>{
+        location.href='https://m.bunjang.co.kr/'
+    }
+)
+document.querySelector('.naver').addEventListener(
+    'click', ()=>{
+        location.href='https://web.joongna.com/'
+    }
+)
